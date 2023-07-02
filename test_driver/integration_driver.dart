ec14fd0258fda543c5cc5d66fbb78aefcf586ec8
a7madZ3Dev <ahmadalzoubi6446@gmail.com>
2023-07-03 00:39:54 +0400

    update libraries

--- test_driver/integration_driver.dart
+++ test_driver/integration_driver.dart
@@ -1,3 +1,6 @@
 import 'package:integration_test/integration_test_driver.dart';
 
 Future<void> main() => integrationDriver();
+
+/// for run integration tests:
+/// flutter driver --driver=test_driver/integration_driver.dart --target=integration_test/app_test.dart
